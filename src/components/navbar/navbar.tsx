1f0e7a8e373ff484c53524ce14b38e46d3fa0b4b
Gra1gKolodziejczyk <graig.kolodziejczyk@ynov.com>
2025-10-05 16:00:09 +0200

    change icon navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,6 @@
 import ThemeToggle from "./theme-toggle";
 import NavLink from "./nav-link";
 import { Button } from "../ui/button";
-import reactImage from "@/assets/react.svg";
 import MobileMenu from "./mobile-menu";
 import ContactDock from "@/components/navbar/contact-dock.tsx";
 
@@ -11,7 +10,7 @@ export default function Navbar() {
       <header className="fixed top-0 z-50 w-full border-b bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4 sm:px-6">
           <a href="#home" className="font-bold tracking-tight text-lg">
-            <img src={reactImage} alt="Logo" className="h-10 w-10" />
+            <img src="/react.svg" alt="Logo" className="h-10 w-10" />
           </a>
 
           <nav className="hidden md:flex items-center gap-6 text-sm font-medium">
